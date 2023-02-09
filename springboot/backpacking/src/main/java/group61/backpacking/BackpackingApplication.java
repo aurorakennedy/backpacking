2bbeb1cde51e8634968548e2c300f61bd99c0eac
Emil Bostrøm Celius <emilbce@stud.ntnu.no>
2023-02-09 11:40:30 +0100

    build: Add basic setup for Spring Boot
    Made using Spring Initializr

---
+++ springboot/backpacking/src/main/java/group61/backpacking/BackpackingApplication.java
@@ -0,0 +1,13 @@
+package group61.backpacking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackpackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackpackingApplication.class, args);
+	}
+
+}
