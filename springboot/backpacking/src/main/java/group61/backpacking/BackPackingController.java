308bed0cd87500b96e4821eb61769cf92489d9dd
tobias.fremming <tobias.fremming@gmail.com>
2023-02-10 13:02:12 +0100

    forgot to stage changes in last commit

--- springboot/backpacking/src/main/java/group61/backpacking/BackPackingController.java
+++ springboot/backpacking/src/main/java/group61/backpacking/BackPackingController.java
@@ -1,5 +1,7 @@
 package group61.backpacking;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,5 +12,22 @@ public class BackPackingController {
 
     @Autowired
     private BackPackingRepository rep;
+
+    @PostMapping("/save")
+    public List<User> saveUser( User inUser){
+        return rep.saveUser(inUser);
+    }
+
+    @GetMapping("/load")
+    public List<User> loadUsers(){
+        return rep.loadUsers();
+    }
+
+    @GetMapping("/delete")
+    public void deleteUser(User inUser){
+        rep.deleteUser(inUser);
+
+
+    }
     
 }
