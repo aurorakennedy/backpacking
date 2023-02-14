b13e4422591f1efc20735807dc874133bd656c70
Emil Bostrøm Celius <emilbce@stud.ntnu.no>
2023-02-14 17:52:49 +0100

    feat(#23): Add methods that handle HTTP requests for user story 1
    Adds methods that will be used to access the database

--- springboot/backpacking/src/main/java/group61/backpacking/BackPackingController.java
+++ springboot/backpacking/src/main/java/group61/backpacking/BackPackingController.java
@@ -3,8 +3,13 @@ package group61.backpacking;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,20 +19,50 @@ public class BackPackingController {
     private BackPackingRepository rep;
 
     @PostMapping("/save")
-    public List<User> saveUser( User inUser){
-        return rep.saveUser(inUser);
+    public List<User> saveUser(User inUser){
+        return null;
+        //return rep.saveUser(inUser);
     }
 
     @GetMapping("/load")
     public List<User> loadUsers(){
-        return rep.loadUsers();
+        return null;
+        //return rep.loadUsers();
     }
 
     @GetMapping("/delete")
     public void deleteUser(User inUser){
-        rep.deleteUser(inUser);
+        //rep.deleteUser(inUser);
+    }
 
+    // Suggestions:
+
+    @PostMapping("/register")
+    public void register(@RequestBody User user) {
 
     }
+
+    @PostMapping("/login")
+    public boolean login(@RequestBody String[] request) {
+        boolean loggedIn = false;
+        return loggedIn;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+
+    }
+
+    @PutMapping("/users/{id}")
+    public void updateUser(@PathVariable int id, @RequestBody User user) {
+
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable int id) {
+        return null;
+    }
+
+
     
 }
