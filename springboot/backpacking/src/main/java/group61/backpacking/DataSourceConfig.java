140987d031849c76bb523121535c31c639f8f8d1
tobias.fremming <tobias.fremming@gmail.com>
2023-02-17 13:59:48 +0100

    forgot a file

--- springboot/backpacking/src/main/java/group61/backpacking/DataSourceConfig.java
+++ springboot/backpacking/src/main/java/group61/backpacking/DataSourceConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
