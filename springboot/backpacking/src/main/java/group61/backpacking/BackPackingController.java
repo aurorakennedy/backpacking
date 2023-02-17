c0329efa29a46cd81b47664a84dc9f40d5ecfbe1
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-02-17 14:15:26 +0100

    fix: Add CrossOrigin and comment out in controller
    Unfinished code commented out.
    
    #23

--- springboot/backpacking/src/main/java/group61/backpacking/BackPackingController.java
+++ springboot/backpacking/src/main/java/group61/backpacking/BackPackingController.java
@@ -19,102 +19,111 @@ public class BackPackingController {
     @Autowired
     private BackPackingRepository rep;
 
-    //@CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "*")
     @PostMapping("/save")
-    public User saveUser(User inUser){
+    public User saveUser(User inUser) {
         try {
             return rep.saveUser(inUser);
 
         } catch (Exception e) {
             return null;
         }
-
-
     }
 
-   // @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "*")
     @GetMapping("/load")
-    public User loadUser(String email)  {
+    public User loadUser(String email) {
         try {
             return rep.loadUser(email);
         } catch (Exception e) {
             return null;
         }
-        
 
     }
-    
+
     @CrossOrigin(origins = "*")
     @GetMapping("/delete")
-    public void deleteUser(User inUser){
+    public void deleteUser(User inUser) {
         rep.deleteUser(inUser);
     }
 
-
+    @CrossOrigin(origins = "*")
     @PostMapping("/login")
     public User login(@RequestBody User user) {
-        
+        System.out.println("Login request recieved on backend.");
         try {
             return rep.login(user);
         } catch (Exception e) {
+            System.out.println(e.toString());
             // TODO: handle exception
             return null;
         }
     }
 
-
     // Suggestions:
 
-  //  @CrossOrigin(origins = "*")
-  //Create a new user
+    // Create a new user
+    @CrossOrigin(origins = "*")
     @PostMapping("/register")
     public void register(@RequestBody User user) {
-        rep.createUser(user.getEmail(), user.getPassword(), user.getUserName());
-        //User savedUser = rep.saveUser(user);
-        
-        System.out.println("Registetration Accepted");
+        User savedUser = rep.saveUser(user);
         System.out.println(user.getUserName());
         System.out.println(user.getEmail());
         System.out.println(user.getPassword());
     }
 
+    /*
+     * @PostMapping("/login")
+     * public boolean login_2(@RequestBody User user) {
+     * return rep.login(user);
+     * //boolean loggedIn = false;
+     * //return loggedIn;
+     * }
+     */
 
-    @PostMapping("/login")
-    public boolean login_2(@RequestBody User user) {
-        return rep.login(user);
-        //boolean loggedIn = false;
-        //return loggedIn;
+    /*
+     * @DeleteMapping("/users/{id}")
+     * public void deleteTheUser(@RequestBody User user) {
+     * 
+     * public void deleteUser(@PathVariable int id) {
+     * // rep.deleteAllByIdInBatch(Iterable<ID> ids)
+     * }
+     */
+
+    @CrossOrigin(origins = "*")
+    @PutMapping("/users/{id}")
+    public void updateUser(@PathVariable int id, @RequestBody User user) {
+
     }
 
-    @DeleteMapping("/users/{id}")
-    public void deleteTheUser(@RequestBody User user) {
+    public void deleteUser(@PathVariable int id, @RequestBody User user) {
+        // rep.deleteAllByIdInBatch(Iterable<ID> ids)
         rep.deleteUser(user);
     }
 
     @PostMapping("/users/{id}")
-    public Boolean updateUser(@RequestBody User user, @RequestBody String password,@RequestBody String userName) {
+    public Boolean updateUser(@RequestBody User user, @RequestBody String password, @RequestBody String userName) {
 
         User updatedUser = rep.updateUser(user, password, userName);
 
         if (user.getUserName() != updatedUser.getUserName()) {
             return false;
-        } 
-        if (user.getPassword() != updatedUser.getPassword()){
+        }
+        if (user.getPassword() != updatedUser.getPassword()) {
             return false;
         }
         return true;
-        }
+    }
 
-        
-    //public void updateUser(){
-    //User user = rep.loadUser(email).orElseThrow(() -> new ResourceNotFoundException("User not exist with id: " + id));
+    // public void updateUser(){
+    // User user = rep.loadUser(email).orElseThrow(() -> new
+    // ResourceNotFoundException("User not exist with id: " + id));
 
     @GetMapping("/users/{id}")
     public User getUserById(@RequestBody User user) {
         return rep.loadUser(user.getEmail());
-        //return new User("test@test.no", "123", "Jarl");
+
+        // return new User("test@test.no", "123", "Jarl");
     }
 
-
-    
 }
