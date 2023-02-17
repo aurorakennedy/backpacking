f9f3a98b2e7120317891cfea8243ffd6b13b03d5
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-02-17 09:59:48 +0100

    fix: Add crossorigin to controller methods
    To get access to SpringBoot functions when sending http-requests.
    
    #23 co-author: Emil Bostrøm Celius <emilbce@stud.ntnu.no>

--- springboot/backpacking/src/main/java/group61/backpacking/BackPackingController.java
+++ springboot/backpacking/src/main/java/group61/backpacking/BackPackingController.java
@@ -3,6 +3,7 @@ package group61.backpacking;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -18,62 +19,65 @@ public class BackPackingController {
     @Autowired
     private BackPackingRepository rep;
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/save")
-    public List<User> saveUser(User inUser){
+    public List<User> saveUser(User inUser) {
         return null;
-        //return rep.saveUser(inUser);
-        //return rep.findAll(User);
+        // return rep.saveUser(inUser);
+        // return rep.findAll(User);
 
     }
 
-    
-
-    
+    @CrossOrigin(origins = "*")
     @GetMapping("/load")
-    public List<User> loadUsers(){
+    public List<User> loadUsers() {
         return null;
-        //return rep.loadUsers();
-        
+        // return rep.loadUsers();
 
     }
-    
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/delete")
-    public void deleteUser(User inUser){
-        //rep.deleteUser(inUser);
+    public void deleteUser(User inUser) {
+        // rep.deleteUser(inUser);
 
     }
-
 
     // Suggestions:
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/register")
     public void register(@RequestBody User user) {
-        //User savedUser = rep.save(user);  
+        // User savedUser = rep.save(user);
+        System.out.println("Register http recieved");
+        System.out.println(user.getUserName());
+        System.out.println(user.getEmail());
+        System.out.println(user.getPassword());
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/login")
     public boolean login(@RequestBody String[] request) {
         boolean loggedIn = false;
         return loggedIn;
     }
 
+    @CrossOrigin(origins = "*")
     @DeleteMapping("/users/{id}")
     public void deleteUser(@PathVariable int id) {
-        //rep.deleteAllByIdInBatch(Iterable<ID> ids)
+        // rep.deleteAllByIdInBatch(Iterable<ID> ids)
     }
 
+    @CrossOrigin(origins = "*")
     @PutMapping("/users/{id}")
     public void updateUser(@PathVariable int id, @RequestBody User user) {
-
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/users/{id}")
     public User getUserById(@PathVariable int id) {
-        //return rep.findOne(id); 
-        return null;
+        // return rep.findOne(id);
+        return new User("test@test.no", "123", "Jarl");
     }
 
-
-    
 }
