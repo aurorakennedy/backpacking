fadfaffaf7b48595bc16ff82a540130a36c36b7d
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-02-28 15:33:43 +0100

    style: Formating of httpRequests file

--- ui/bp-advisor/src/components/httpRequests.ts
+++ ui/bp-advisor/src/components/httpRequests.ts
@@ -1,153 +1,172 @@
-import { Itinerary, ItineraryDestinationJoined, User } from "./types";
+import { Itinerary, ItineraryDestination, User } from "./types";
 
 async function getUser(userId: number): Promise<User> {
-    const response: Response = await fetch(`http://localhost:8080/users/${userId}`);
-    if (!response.ok) {
-        throw new Error('Failed to fetch user');
-    }
-    const user: User = await response.json();
-    return user;
+  const response: Response = await fetch(
+    `http://localhost:8080/users/${userId}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch user");
+  }
+  const user: User = await response.json();
+  return user;
 }
 
 async function register(user: User): Promise<User> {
-    const response: Response = await fetch('http://localhost:8080/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-    });
-    if (!response.ok) {
-        throw new Error('Failed to register user');
-    }
-    try {
-        const loggedInUser: User = await response.json();
-        return loggedInUser;
-    } catch (error) {
-        //THIS SHOULD BE FIXED: UGLY!
-        return {
-            username: "failed",
-            email: "failed",
-            password: "failed"
-        };
-    }
+  const response: Response = await fetch("http://localhost:8080/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+  if (!response.ok) {
+    throw new Error("Failed to register user");
+  }
+  try {
+    const loggedInUser: User = await response.json();
+    return loggedInUser;
+  } catch (error) {
+    //THIS SHOULD BE FIXED: UGLY!
+    return {
+      username: "failed",
+      email: "failed",
+      password: "failed",
+    };
+  }
 }
 
 async function login(user: User): Promise<User> {
-    const response: Response = await fetch('http://localhost:8080/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-    });
-    console.log("Response: " + response.status);
-    if (!response.ok) {
-        throw new Error('Failed to log in');
-    }
-    try {
-        const loggedInUser: User = await response.json();
-        return loggedInUser;
-    } catch (error) {
-        //THIS SHOULD BE FIXED: UGLY!
-        return {
-            username: "failed",
-            email: "failed",
-            password: "failed"
-        };
-    }
+  const response: Response = await fetch("http://localhost:8080/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+  console.log("Response: " + response.status);
+  if (!response.ok) {
+    throw new Error("Failed to log in");
+  }
+  try {
+    const loggedInUser: User = await response.json();
+    return loggedInUser;
+  } catch (error) {
+    //THIS SHOULD BE FIXED: UGLY!
+    return {
+      username: "failed",
+      email: "failed",
+      password: "failed",
+    };
+  }
 }
 
 async function updateUser(userId: number, user: User): Promise<void> {
-    const response: Response = await fetch(`http://localhost:8080/users/${userId}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-    });
-    if (!response.ok) {
-        throw new Error('Failed to update user');
+  const response: Response = await fetch(
+    `http://localhost:8080/users/${userId}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
     }
+  );
+  if (!response.ok) {
+    throw new Error("Failed to update user");
+  }
 }
 
 async function deleteUser(userId: number): Promise<void> {
-    const response: Response = await fetch(`http://localhost:8080/users/${userId}`, {
-        method: 'DELETE',
-    });
-    if (!response.ok) {
-        throw new Error('Failed to delete user');
+  const response: Response = await fetch(
+    `http://localhost:8080/users/${userId}`,
+    {
+      method: "DELETE",
     }
+  );
+  if (!response.ok) {
+    throw new Error("Failed to delete user");
+  }
 }
 
 async function getItineraryById(itineraryId: number): Promise<Itinerary> {
-    const response: Response = await fetch(`http://localhost:8080/itinerary/${itineraryId}`);
-    if (!response.ok) {
-        throw new Error('Failed to fetch itinerary');
-    }
-    const itinerary: Itinerary = await response.json();
-    return itinerary;
+  const response: Response = await fetch(
+    `http://localhost:8080/itinerary/${itineraryId}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch itinerary");
+  }
+  const itinerary: Itinerary = await response.json();
+  return itinerary;
 }
 
-async function getItinerariesByUserEmail(userEmail: string): Promise<Itinerary[]> {
-    const response: Response = await fetch(`http://localhost:8080/itineraries/${userEmail}`);
-    if (!response.ok) {
-        throw new Error('Failed to fetch itineraries');
-    }
-    const itineraries: Itinerary[] = await response.json();
-    return itineraries;
+async function getItinerariesByUserEmail(
+  userEmail: string
+): Promise<Itinerary[]> {
+  const response: Response = await fetch(
+    `http://localhost:8080/itineraries/${userEmail}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch itineraries");
+  }
+  const itineraries: Itinerary[] = await response.json();
+  return itineraries;
 }
-
 
 async function addItinerary(itinerary: Itinerary): Promise<void> {
-    const response: Response = await fetch('http://localhost:8080/itinerary', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(itinerary),
-    });
-    if (!response.ok) {
-        throw new Error('Failed to add itinerary');
-    }
+  const response: Response = await fetch("http://localhost:8080/itinerary", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(itinerary),
+  });
+  if (!response.ok) {
+    throw new Error("Failed to add itinerary");
+  }
 }
 
-async function getItineraryDestinationsJoined(itineraryID: number):
-    Promise<ItineraryDestinationJoined[]> {
-    const response: Response = await
-        fetch(`http://localhost:8080/itinerarydestinations/${itineraryID}`);
-    if (!response.ok) {
-        throw new Error('Failed to fetch itinerary destinations');
-    }
-    const itineraryDestinations: ItineraryDestinationJoined[] = await response.json();
-    return itineraryDestinations;
+async function getItineraryDestinationsJoined(
+  itineraryID: number
+): Promise<ItineraryDestination[]> {
+  const response: Response = await fetch(
+    `http://localhost:8080/itinerarydestinations/${itineraryID}`
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch itinerary destinations");
+  }
+  const itineraryDestinations: ItineraryDestination[] = await response.json();
+  return itineraryDestinations;
 }
 
-async function addItineraryDestinationsJoined(itineraryDestinations:
-    ItineraryDestinationJoined[]): Promise<void> {
-    const response: Response = await fetch('http://localhost:8080/itinerarydestinations', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(itineraryDestinations),
-    });
-    if (!response.ok) {
-        throw new Error('Failed to add itinerary destinations');
+async function addItineraryDestinationsJoined(
+  itineraryDestinations: ItineraryDestination[]
+): Promise<void> {
+  const response: Response = await fetch(
+    "http://localhost:8080/itinerarydestinations",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(itineraryDestinations),
     }
+  );
+  if (!response.ok) {
+    throw new Error("Failed to add itinerary destinations");
+  }
 }
 
 const httpRequests = {
-    getUser,
-    register,
-    login,
-    updateUser,
-    deleteUser,
-    addItinerary,
-    addItineraryDestinationsJoined,
-    getItineraryById,
-    getItinerariesByUserEmail,
-    getItineraryDestinationsJoined,
-}
+  getUser,
+  register,
+  login,
+  updateUser,
+  deleteUser,
+  addItinerary,
+  addItineraryDestinationsJoined,
+  getItineraryById,
+  getItinerariesByUserEmail,
+  getItineraryDestinationsJoined,
+};
 
 export default httpRequests;
