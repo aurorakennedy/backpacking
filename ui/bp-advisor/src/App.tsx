74e9e217680a6d8a3dc5a271d9643f94c0f93614
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-02-28 15:32:41 +0100

    fix: Fix format of types
    Types matching new database format.

--- ui/bp-advisor/src/App.tsx
+++ ui/bp-advisor/src/App.tsx
@@ -1,55 +1,76 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { LoggedInUser } from './components/types';
-import LogInBox from './components/LogInBox';
-import React, { useEffect, useState } from 'react';
-import SignUpBox from './components/SignUpBox';
-import HomePage from './components/HomePage';
-import CreateNewItineraryForm from './components/CreateNewItineraryForm';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { LoggedInUser } from "./components/types";
+import LogInBox from "./components/LogInBox";
+import React, { useEffect, useState } from "react";
+import SignUpBox from "./components/SignUpBox";
+import HomePage from "./components/HomePage";
+import CreateNewItineraryForm from "./components/CreateNewItineraryForm";
 
 function App() {
+  // Keeps track of the logged in user.
+  // When the page is loaded, checks the browsers storage to see whether a user is already logged in.
+  const [loggedInUser, setLoggedInUser] = useState<LoggedInUser | null>(
+    JSON.parse(localStorage.getItem("loggedInUser") || "null")
+  );
 
-    // Keeps track of the logged in user.
-    // When the page is loaded, checks the browsers storage to see whether a user is already logged in.
-    const [loggedInUser, setLoggedInUser] = useState<LoggedInUser | null>(
-        JSON.parse(localStorage.getItem('loggedInUser') || 'null')
-    );
+  // Saves the logged in user to the browser storage when user logges in.
+  useEffect(() => {
+    localStorage.setItem("loggedInUser", JSON.stringify(loggedInUser));
+  }, [loggedInUser]);
 
-
-    // Saves the logged in user to the browser storage when user logges in.
-    useEffect(() => {
-        localStorage.setItem('loggedInUser', JSON.stringify(loggedInUser));
-    }, [loggedInUser]);
-
-
-
-    return (
-        <Router>
-            <div className="App">
-
-                {loggedInUser ? (
-                    <>
-                        <Routes>
-                            <Route path='/' element={<HomePage setLoggedInUser={setLoggedInUser} />} />
-                            <Route path='/logIn' element={<HomePage setLoggedInUser={setLoggedInUser} />} />
-                            <Route path='/signUp' element={<HomePage setLoggedInUser={setLoggedInUser} />} />
-                            <Route path='/homePage' element={<HomePage setLoggedInUser={setLoggedInUser} />} />
-                            <Route path='/createItinerary' element={<CreateNewItineraryForm setLoggedInUser={setLoggedInUser} />} />
-                        </Routes>
-                    </>
-                ) : (
-                    <Routes>
-                        <Route path='/' element={<LogInBox setLoggedInUser={setLoggedInUser} />} />
-                        <Route path='/signUp' element={<SignUpBox setLoggedInUser={setLoggedInUser} />} />
-                        <Route path='/logIn' element={<LogInBox setLoggedInUser={setLoggedInUser} />} />
-                    </Routes>
-                )}
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        {loggedInUser ? (
+          <>
+            <Routes>
+              <Route
+                path="/"
+                element={<HomePage setLoggedInUser={setLoggedInUser} />}
+              />
+              <Route
+                path="/logIn"
+                element={<HomePage setLoggedInUser={setLoggedInUser} />}
+              />
+              <Route
+                path="/signUp"
+                element={<HomePage setLoggedInUser={setLoggedInUser} />}
+              />
+              <Route
+                path="/homePage"
+                element={<HomePage setLoggedInUser={setLoggedInUser} />}
+              />
+              <Route
+                path="/createItinerary"
+                element={
+                  <CreateNewItineraryForm
+                    setLoggedInUser={setLoggedInUser}
+                    loggedInUser={loggedInUser}
+                  />
+                }
+              />
+            </Routes>
+          </>
+        ) : (
+          <Routes>
+            <Route
+              path="/"
+              element={<LogInBox setLoggedInUser={setLoggedInUser} />}
+            />
+            <Route
+              path="/signUp"
+              element={<SignUpBox setLoggedInUser={setLoggedInUser} />}
+            />
+            <Route
+              path="/logIn"
+              element={<LogInBox setLoggedInUser={setLoggedInUser} />}
+            />
+          </Routes>
+        )}
+      </div>
+    </Router>
+  );
 }
 
 export default App;
-
-
--- ui/bp-advisor/src/components/CreateNewItineraryForm.tsx
+++ ui/bp-advisor/src/components/CreateNewItineraryForm.tsx
@@ -1,165 +1,250 @@
-import './createNewItineraryFormStyle.css'
-import { Destination, ItineraryDestinationJoined, LoggedInUser } from './types';
-import httpRequests from './httpRequests';
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Nav from './NavBar';
-
+import "./createNewItineraryFormStyle.css";
+import { Destination, ItineraryDestination, LoggedInUser } from "./types";
+import httpRequests from "./httpRequests";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Nav from "./NavBar";
 
 type CreateNewItineraryFormProps = {
-    setLoggedInUser: React.Dispatch<React.SetStateAction<LoggedInUser | null>>
-}
-
+  setLoggedInUser: React.Dispatch<React.SetStateAction<LoggedInUser | null>>;
+  loggedInUser: LoggedInUser;
+};
 
 /**
- * Component for BP-Advisor to create new itineraries, including relevant input fields, 
+ * Component for BP-Advisor to create new itineraries, including relevant input fields,
  * a submit button, and functions for sending the data to the back end.
- * 
+ *
  * @returns HTML-code for a BP-Advisor login box and functions to support login.
  */
-const CreateNewItineraryForm = ({ setLoggedInUser }: CreateNewItineraryFormProps) => {
+const CreateNewItineraryForm = ({
+  loggedInUser,
+  setLoggedInUser,
+}: CreateNewItineraryFormProps) => {
+  // Saves a list of the destinations added
+  const [destinations] = useState<Destination[]>([]);
 
-    // Saves a list of the destinations added
-    const [destinations] = useState<Destination[]>([]);
+  // Adds a destination to the state
+  function addDestinationToState(
+    destinationName: string,
+    countryName: string
+  ): void {
+    destinations.push({
+      name: destinationName,
+      country: countryName,
+      description: "",
+    });
+  }
 
-    // Adds a destination to the state
-    function addDestinationToState(destinationName: string, countryName: string): void {
-        destinations.push({ id: 0, name: destinationName, area: "", country: countryName });
+  /**
+   * Adds a destination to the state and to the DOM
+   * @returns void, if destination inputs are not provided
+   */
+  function handleAddDestination(): React.MouseEventHandler<HTMLButtonElement> | void {
+    let destinationsAsHTML = document.getElementById(
+      "destinationsAsHTML"
+    ) as HTMLDivElement;
+    let destinationName: string = (
+      document.getElementById("destinationNameInput") as HTMLInputElement
+    ).value;
+    let countryName: string = (
+      document.getElementById("countryInput") as HTMLInputElement
+    ).value;
+
+    if (destinationName.length < 1 || countryName.length < 1) {
+      alert("The destination needs both a name and a country");
+      return;
     }
 
-    /**
-     * Adds a destination to the state and to the DOM
-     * @returns void, if destination inputs are not provided
-     */
-    function handleAddDestination(): React.MouseEventHandler<HTMLButtonElement> | void {
+    addDestinationToState(destinationName, countryName);
 
-        let destinationsAsHTML = document.getElementById("destinationsAsHTML") as HTMLDivElement;
-        let destinationName: string = (document.getElementById('destinationNameInput') as HTMLInputElement).value;
-        let countryName: string = (document.getElementById('countryInput') as HTMLInputElement).value;
+    let p = document.createElement("p");
+    p.textContent = destinationName + " - " + countryName;
+    destinationsAsHTML.appendChild(p);
 
-        if (destinationName.length < 1 || countryName.length < 1) {
-            alert("The destination needs both a name and a country");
-            return;
-        }
+    console.log(destinations);
+  }
 
-        addDestinationToState(destinationName, countryName);
+  /**
+   * Gets the information form the input fields and sends it as http-requests to the backend for saving
+   * in the database.
+   */
+  function submitDestinationInfo():
+    | React.MouseEventHandler<HTMLButtonElement>
+    | any {
+    const titleInputValue: string = (
+      document.getElementById("titleInput") as HTMLInputElement
+    ).value;
+    const timeInputValue: string = (
+      document.getElementById("estimatedTimeInput") as HTMLInputElement
+    ).value;
+    const priceInputValue: string = (
+      document.getElementById("estimatedPriceInput") as HTMLInputElement
+    ).value;
+    const descriptionInputValue: string = (
+      document.getElementById("descriptionInput") as HTMLInputElement
+    ).value;
 
-        let p = document.createElement('p');
-        p.textContent = destinationName + " - " + countryName;
-        destinationsAsHTML.appendChild(p);
-
-        console.log(destinations);
+    if (
+      titleInputValue.length < 1 ||
+      timeInputValue.length < 1 ||
+      priceInputValue.length < 1 ||
+      descriptionInputValue.length < 1
+    ) {
+      alert("All fields must be filled out");
+      return;
     }
 
-    /**
-     * Gets the information form the input fields and sends it as http-requests to the backend for saving
-     * in the database.
-     */
-    function submitDestinationInfo(): React.MouseEventHandler<HTMLButtonElement> | any {
-
-        const titleInputValue: string = (document.getElementById('titleInput') as HTMLInputElement).value;
-        const timeInputValue: string = (document.getElementById('estimatedTimeInput') as HTMLInputElement).value;
-        const priceInputValue: string = (document.getElementById('estimatedPriceInput') as HTMLInputElement).value;
-        const descriptionInputValue: string = (document.getElementById('descriptionInput') as HTMLInputElement).value;
-
-        if (titleInputValue.length < 1 || timeInputValue.length < 1 || priceInputValue.length < 1 || descriptionInputValue.length < 1) {
-            alert('All fields must be filled out')
-            return;
-        }
-
-        if (destinations.length < 1) {
-            alert('You must have added at least one destination')
-            return;
-        }
-
-        const createdId: number = Math.random();
-
-        const itineraryDestinations: ItineraryDestinationJoined[] = [];
-
-        let orderOfDestination = 0;
-
-        destinations.forEach(destination => {
-
-            itineraryDestinations.push({
-                itineraryID: createdId,
-                destinationID: 0, // Must be set in backend when checked with database
-                name: destination.name,
-                area: "",
-                country: destination.country,
-                continent: "",
-                order: orderOfDestination
-            });
-            orderOfDestination++;
-        });
-
-        try {
-            httpRequests.addItinerary({
-                id: createdId,
-                name: titleInputValue,
-                cost: +priceInputValue,
-                distance: 0,
-                duration: +timeInputValue,
-                description: descriptionInputValue
-            });
-            httpRequests.addItineraryDestinationsJoined(itineraryDestinations);
-            window.location.reload();
-            window.location.replace('/homePage');
-        } catch (error) {
-            alert("There was an error when trying to add the route, please try again.");
-        }
-
+    if (destinations.length < 1) {
+      alert("You must have added at least one destination");
+      return;
     }
 
-    return (
-        <>
-            <Nav setLoggedInUser={setLoggedInUser} />
-            <div id='newRouteBox'>
-                <form>
-                    <h2> Add a backpacking itinerary</h2>
-                    <Link id='cancelButton' to='/homePage'>
-                        Cancel
-                    </Link>
-                    <br></br>
-                    <label className='newRouteLabel' id='titleInputLabel'>Title</label>
-                    <input className='newRouteInput' id='titleInput' type="input" placeholder='   ...'></input>
-                    <br></br>
-                    <label className='newRouteLabel' id='estimatedTimeInputLabel'>Estimated time (in days)</label>
-                    <input className='newRouteInput' id='estimatedTimeInput' type='input' placeholder='   ...'></input>
-                    <br></br>
-                    <label className='newRouteLabel' id='estimatedPriceInputLabel'>Estimated price (in dollars)</label>
-                    <input className='newRouteInput' id='estimatedPriceInput' type='input' placeholder='   ...'></input>
-                    <br></br>
-                    <label className='newRouteLabel' id='descriptionInputLabel'> Description </label>
-                    <textarea className='newRouteInput' id='descriptionInput' placeholder='   ...'></textarea>
-                    <br></br>
-                    <br></br>
-                    <label className='newRouteLabel' id='destinationInputLabel'>Add destination to itinerary</label>
-                    <br></br>
-                    <div id='destinationInputBox'>
-                        <div>
-                            <label className='newRouteLabel' id='destinationNameInputLabel'>Destination name</label>
-                            <input className='newRouteInput' id='destinationNameInput' type='input' placeholder='   ...'></input>
-                        </div>
-                        <div>
-                            <label className='newRouteLabel' id='countryInputLabel'>Country of destination</label>
-                            <input className='newRouteInput' id='countryInput' type='input' placeholder='   ...'></input>
-                        </div>
-                    </div>
-                    <br></br>
-                    <button id='addDestinationButton' onClick={handleAddDestination} type='button'> Add destination</button>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <div id='destinationsAsHTML'>
-                        <p id='destinationsAsHTMLTitle'>Destinations added:</p>
-                        <br></br>
-                        <br></br>
+    const createdId: number = Math.random();
 
-                    </div>
-                    <button id='submitItineraryButton' onClick={submitDestinationInfo} type='button'> Add route</button>
-                </form>
+    const itineraryDestinations: ItineraryDestination[] = [];
+
+    let orderOfDestination = 0;
+
+    destinations.forEach((destination) => {
+      itineraryDestinations.push({
+        itineraryID: createdId,
+        order: orderOfDestination,
+        destinationName: destination.name,
+        country: destination.country,
+      });
+      orderOfDestination++;
+    });
+
+    try {
+      console.log(loggedInUser.email);
+      httpRequests.addItinerary({
+        id: createdId,
+        writerEmail: loggedInUser?.email,
+        writtenDate: new Date(),
+        title: titleInputValue,
+        cost: +priceInputValue,
+        estimatedTime: +timeInputValue,
+        description: descriptionInputValue,
+        image: "",
+      });
+      httpRequests.addItineraryDestinationsJoined(itineraryDestinations);
+      window.location.reload();
+      window.location.replace("/homePage");
+    } catch (error) {
+      alert(
+        "There was an error when trying to add the route, please try again."
+      );
+    }
+  }
+
+  return (
+    <>
+      <Nav setLoggedInUser={setLoggedInUser} />
+      <div id="newRouteBox">
+        <form>
+          <h2> Add a backpacking itinerary</h2>
+          <Link id="cancelButton" to="/homePage">
+            Cancel
+          </Link>
+          <br></br>
+          <label className="newRouteLabel" id="titleInputLabel">
+            Title
+          </label>
+          <input
+            className="newRouteInput"
+            id="titleInput"
+            type="input"
+            placeholder="   ..."
+          ></input>
+          <br></br>
+          <label className="newRouteLabel" id="estimatedTimeInputLabel">
+            Estimated time (in days)
+          </label>
+          <input
+            className="newRouteInput"
+            id="estimatedTimeInput"
+            type="input"
+            placeholder="   ..."
+          ></input>
+          <br></br>
+          <label className="newRouteLabel" id="estimatedPriceInputLabel">
+            Estimated price (in dollars)
+          </label>
+          <input
+            className="newRouteInput"
+            id="estimatedPriceInput"
+            type="input"
+            placeholder="   ..."
+          ></input>
+          <br></br>
+          <label className="newRouteLabel" id="descriptionInputLabel">
+            {" "}
+            Description{" "}
+          </label>
+          <textarea
+            className="newRouteInput"
+            id="descriptionInput"
+            placeholder="   ..."
+          ></textarea>
+          <br></br>
+          <br></br>
+          <label className="newRouteLabel" id="destinationInputLabel">
+            Add destination to itinerary
+          </label>
+          <br></br>
+          <div id="destinationInputBox">
+            <div>
+              <label className="newRouteLabel" id="destinationNameInputLabel">
+                Destination name
+              </label>
+              <input
+                className="newRouteInput"
+                id="destinationNameInput"
+                type="input"
+                placeholder="   ..."
+              ></input>
             </div>
-        </>)
-}
+            <div>
+              <label className="newRouteLabel" id="countryInputLabel">
+                Country of destination
+              </label>
+              <input
+                className="newRouteInput"
+                id="countryInput"
+                type="input"
+                placeholder="   ..."
+              ></input>
+            </div>
+          </div>
+          <br></br>
+          <button
+            id="addDestinationButton"
+            onClick={handleAddDestination}
+            type="button"
+          >
+            {" "}
+            Add destination
+          </button>
+          <br></br>
+          <br></br>
+          <br></br>
+          <div id="destinationsAsHTML">
+            <p id="destinationsAsHTMLTitle">Destinations added:</p>
+            <br></br>
+            <br></br>
+          </div>
+          <button
+            id="submitItineraryButton"
+            onClick={submitDestinationInfo}
+            type="button"
+          >
+            {" "}
+            Add route
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default CreateNewItineraryForm;
