5b894ba9e5ad22ed840fc9fc665951f3a522afe0
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-03-03 18:29:58 +0100

    feat: Begin work on updating info
    Info abut itineraries from db to frontend.
    
    #67

--- ui/bp-advisor/src/components/ItineraryListBox.tsx
+++ ui/bp-advisor/src/components/ItineraryListBox.tsx
@@ -1,76 +1,184 @@
 import { useEffect, useState } from "react";
-import TravelRoute from "./TravelRoute"
-import "./itineraryListBoxStyle.css"
-import { Itinerary } from "./types";
+import ItinerarySummaryBox from "./ItinerarySummaryBox";
+import "./itineraryListBoxStyle.css";
+import {
+    Itinerary,
+    ItineraryAndDestinations,
+    LoggedInUser,
+    User,
+} from "./types";
 import httpRequests from "./httpRequests";
+import { createRoot } from "react-dom/client";
 
+type CreateNewItineraryFormProps = {
+    loggedInUser: LoggedInUser;
+};
 
-interface itinerarySummaryInfo {
-    title: string;
-    author: string;
-    duration: string;
-    price: string;
-    description: string;
-}
+const ItineraryListBox = ({ loggedInUser }: CreateNewItineraryFormProps) => {
+    const [itineraryBoxExpanded, setitineraryBoxExpanded] =
+        useState<Boolean>(false);
 
-const mockItinerary1: itinerarySummaryInfo = {
-    title: 'Backpacking South America',
-    author: 'Jarl28',
-    duration: '40 days',
-    price: '$ 3000',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-}
-
-const mockItinerary2: itinerarySummaryInfo = {
-    title: 'Interrail in Europe',
-    author: 'Aurora24',
-    duration: '30 days',
-    price: '$ 2900',
-    description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.'
-}
-
-const mockItinerary3: itinerarySummaryInfo = {
-    title: 'Bikepacking in Asia',
-    author: 'Tobias32',
-    duration: '50 days',
-    price: '$ 3500',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-}
-
-const mockItinerary4: itinerarySummaryInfo = {
-    title: 'USA coast to coast',
-    author: 'Simen20',
-    duration: '45 days',
-    price: '$ 3600',
-    description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.'
-}
-
-
-
-
-const ItineraryListBox = () => {
-
-    const [itineraryBoxExpanded, setitineraryBoxExpanded] = useState<Boolean>(false);
-
-    const handleExpantion = () => {
+    async function updateExpandableItineraryContainerDiv(): Promise<
+        React.MouseEventHandler<HTMLButtonElement> | any
+    > {
         try {
-            const promise: Promise<Itinerary> = httpRequests.getItineraryById(3);
-            promise.then((itinerary: Itinerary) => {
-                console.log(itinerary);
-            })
-            const promise2: Promise<Itinerary[]> = httpRequests.getItinerariesByUserEmail("hello@hello.com");
-            promise2.then((itineraries: Itinerary[]) => {
-                console.log(itineraries);
-            })
+            const promise: Promise<ItineraryAndDestinations[]> =
+                httpRequests.getItinerariesByUserEmail(loggedInUser.email);
+            promise.then((itinerariesOfUser: ItineraryAndDestinations[]) =>
+                itinerariesOfUser
+                    .reverse()
+                    .forEach((itineraryAndDestinations) => {
+                        if (
+                            !document.getElementById(
+                                itineraryAndDestinations.itinerary.id.toString()
+                            )
+                        ) {
+                            let expandableItineraryContainerDiv: HTMLDivElement =
+                                document.getElementById(
+                                    "expandableItineraryContainer"
+                                ) as HTMLDivElement;
+                            let itinerarySummaryDiv: HTMLDivElement =
+                                document.createElement("div");
+                            itinerarySummaryDiv.classList.add(
+                                "itinerarySummaryDiv"
+                            );
+
+                            itinerarySummaryDiv.id =
+                                itineraryAndDestinations.itinerary.id.toString();
+                            itinerarySummaryDiv.addEventListener(
+                                "click",
+                                function (event: MouseEvent) {
+                                    const itinerarySummaryDiv = (
+                                        event.target as HTMLElement
+                                    ).closest(
+                                        ".itinerarySummaryDiv"
+                                    ) as HTMLDivElement;
+                                    if (itinerarySummaryDiv) {
+                                        handleExpantionOpen(
+                                            event,
+                                            itinerarySummaryDiv.id
+                                        );
+                                    }
+                                }
+                            );
+
+                            let description: string = "";
+                            if (
+                                itineraryAndDestinations.itinerary.description
+                                    .length > 150
+                            ) {
+                                description =
+                                    itineraryAndDestinations.itinerary.description.substring(
+                                        0,
+                                        150
+                                    ) + " ...";
+                            } else {
+                                description =
+                                    itineraryAndDestinations.itinerary
+                                        .description;
+                            }
+
+                            let itinerarySummaryBox = (
+                                <ItinerarySummaryBox
+                                    title={
+                                        itineraryAndDestinations.itinerary.title
+                                    }
+                                    description={description}
+                                    estimatedTime={itineraryAndDestinations.itinerary.estimatedTime.toString()}
+                                    cost={itineraryAndDestinations.itinerary.cost.toString()}
+                                />
+                            );
+
+                            createRoot(itinerarySummaryDiv).render(
+                                itinerarySummaryBox
+                            );
+                            expandableItineraryContainerDiv.appendChild(
+                                itinerarySummaryDiv
+                            );
+
+                            console.log(
+                                itineraryAndDestinations.itinerary.id.toString()
+                            );
+                        }
+                    })
+            );
         } catch (error) {
-            alert('Error!!!');
+            alert("Could not load itineraries. Please refresh the page");
         }
-        setitineraryBoxExpanded(!itineraryBoxExpanded);
+    }
+
+    async function handleExpantionOpen(
+        event: MouseEvent,
+        itineraryId: string
+    ): Promise<React.MouseEventHandler<HTMLElement> | any> {
+        try {
+            const promise: Promise<ItineraryAndDestinations> =
+                httpRequests.getItineraryAndDestinationsById(
+                    parseInt(itineraryId)
+                );
+            promise.then(
+                (itineraryAndDestinations: ItineraryAndDestinations) => {
+                    let title: HTMLElement = document.getElementById(
+                        "itineraryBoxTitle"
+                    ) as HTMLElement;
+                    title.innerHTML = itineraryAndDestinations.itinerary.title;
+
+                    let author: HTMLElement = document.getElementById(
+                        "itineraryDetailsAuthor"
+                    ) as HTMLElement;
+
+                    /* try {
+                        const userPromise: Promise<User> = httpRequests.getUser(
+                            itineraryAndDestinations.itinerary.writerEmail
+                        );
+                        userPromise.then((user: User) => {
+                            console.log(username);
+                            username = user.username;
+                        });
+                    } catch (error) {
+                        //TODO: Error handling
+                    } */
+
+                    author.innerHTML =
+                        "Author: " +
+                        itineraryAndDestinations.itinerary.writerEmail;
+
+                    let duration: HTMLElement = document.getElementById(
+                        "itineraryDetailsDuration"
+                    ) as HTMLElement;
+
+                    duration.innerHTML =
+                        "Duration: " +
+                        itineraryAndDestinations.itinerary.estimatedTime;
+
+                    let cost: HTMLElement = document.getElementById(
+                        "itineraryDetailsCost"
+                    ) as HTMLElement;
+
+                    cost.innerHTML =
+                        "Cost: " + itineraryAndDestinations.itinerary.cost;
+
+                    let itineraryBoxDescription: HTMLElement =
+                        document.getElementById(
+                            "itineraryBoxDescription"
+                        ) as HTMLElement;
+
+                    itineraryBoxDescription.innerHTML =
+                        itineraryAndDestinations.itinerary.description;
+                }
+            );
+        } catch (error) {}
+
+        setitineraryBoxExpanded(true);
+    }
+
+    const handleExpantionClose = () => {
+        setitineraryBoxExpanded(false);
     };
 
-    /* useEffect(() => {
-        setDestinations(["Peru", "Chile", "Argentina"]);
-    }, []); */
+    useEffect(() => {
+        updateExpandableItineraryContainerDiv();
+    }, []);
 
     /* const destinationsHTMLElement: HTMLDivElement = document.getElementById('itineraryDestinationBox') as HTMLDivElement;
 
@@ -84,69 +192,49 @@ const ItineraryListBox = () => {
 
     return (
         <>
-            <div id='travelRouteDiv'>
-
-                <div onClick={handleExpantion} className="itinerarySummaryDiv">
-                    <TravelRoute
-                        title={mockItinerary1.title}
-                        description={mockItinerary1.description}
-                        duration={mockItinerary1.duration}
-                        price={mockItinerary1.price} /* image={''}      */ />
-                </div>
-
-                <div onClick={handleExpantion} className="itinerarySummaryDiv">
-                    <TravelRoute
-                        title={mockItinerary2.title}
-                        description={mockItinerary2.description}
-                        duration={mockItinerary2.duration}
-                        price={mockItinerary2.price} /* image={''}      */ />
-                </div>
-                <div onClick={handleExpantion} className="itinerarySummaryDiv">
-                    <TravelRoute
-                        title={mockItinerary3.title}
-                        description={mockItinerary3.description}
-                        duration={mockItinerary3.duration}
-                        price={mockItinerary3.price} /* image={''}      */ />
-                </div>
-
-                <div onClick={handleExpantion} className="itinerarySummaryDiv">
-                    <TravelRoute
-                        title={mockItinerary4.title}
-                        description={mockItinerary4.description}
-                        duration={mockItinerary4.duration}
-                        price={mockItinerary4.price} /* image={''}      */ />
-                </div>
-            </div>
-            {!itineraryBoxExpanded ? (<></>)
-                : (<><div id="itineraryBox">
-                    <div id="itineraryDestinationBox">
-                        <p id='itineraryDestinationBoxTitle'> Destinations: </p>
-                        <p>Chile</p>
-                        <p>Argentina</p>
-                        <p>Peru</p>
-                    </div>
-                    <div id="itineraryColumnFlexBox">
-                        <h2 id='itineraryBoxTitle'> {'Backpacking South America'}</h2>
-                        <div id='itineraryDetailsFlexBox'>
-                            <p className='itineraryDetailElement'> Author: {"Jarl28"}</p>
-                            <p className='itineraryDetailElement'> Duration: {"40"}</p>
-                            <p className='itineraryDetailElement'> Cost: {"$ 3000"}</p>
+            <div id="expandableItineraryContainer"></div>
+            {!itineraryBoxExpanded ? (
+                <></>
+            ) : (
+                <>
+                    <div id="itineraryBox">
+                        <div id="itineraryDestinationBox">
+                            <p id="itineraryDestinationBoxTitle">
+                                Destinations:
+                            </p>
+                            <p>Chile</p>
+                            <p>Argentina</p>
+                            <p>Peru</p>
                         </div>
-                        <p id='itineraryBoxDescription'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum dolor sit amet consectetur adipiscing.
-                            <br></br>
-                            <br></br>
-                            Nec sagittis aliquam malesuada bibendum arcu vitae elementum. Varius duis at consectetur lorem donec massa sapien. Commodo viverra maecenas accumsan lacus vel facilisis. Aliquet eget sit amet tellus. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit lectus. Condimentum id venenatis a condimentum vitae sapien pellentesque habitant morbi. Tellus pellentesque eu tincidunt tortor aliquam nulla. Scelerisque mauris pellentesque pulvinar pellentesque habitant. Pretium nibh ipsum consequat nisl. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Adipiscing vitae proin sagittis nisl rhoncus.
-                            <br></br>
-                            <br></br>
-
-                            Felis donec et odio pellentesque diam volutpat commodo sed. Dictum fusce ut placerat orci nulla pellentesque dignissim enim sit. Consectetur adipiscing elit pellentesque habitant morbi tristique senectus. Cras adipiscing enim eu turpis egestas. Faucibus in ornare quam viverra orci sagittis eu volutpat. Cursus in hac habitasse platea. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Mauris sit amet massa vitae tortor. Vitae congue mauris rhoncus aenean vel elit scelerisque mauris. Suspendisse in est ante in nibh. Montes nascetur ridiculus mus mauris vitae ultricies. Morbi tincidunt augue interdum velit euismod in pellentesque. Eu lobortis elementum nibh tellus. Amet consectetur adipiscing elit pellentesque habitant morbi. Nunc sed augue lacus viverra vitae congue eu consequat ac. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus.
+                        <div id="itineraryColumnFlexBox">
+                            <h2 id="itineraryBoxTitle"></h2>
+                            <div id="itineraryDetailsFlexBox">
+                                <p
+                                    id="itineraryDetailsAuthor"
+                                    className="itineraryDetailElement"
+                                ></p>
+                                <p
+                                    id="itineraryDetailsDuration"
+                                    className="itineraryDetailElement"
+                                ></p>
+                                <p
+                                    id="itineraryDetailsCost"
+                                    className="itineraryDetailElement"
+                                ></p>
+                            </div>
+                            <p id="itineraryBoxDescription"></p>
+                        </div>
+                        <p
+                            id="itineraryBoxCloseButton"
+                            onClick={handleExpantionClose}
+                        >
+                            Close
                         </p>
                     </div>
-                    <p onClick={handleExpantion} id='itineraryBoxCloseButton'> Close </p>
-                </div></>)}
+                </>
+            )}
         </>
-    )
+    );
+};
 
-}
-
-export default ItineraryListBox
+export default ItineraryListBox;
