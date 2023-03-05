d6854b6a1e5fe2c858448a411c5dffcc6650ef89
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-03-05 21:39:57 +0100

    style: Remove unused imports
    #67

--- springboot/backpacking/src/main/java/group61/backpacking/BackPackingController.java
+++ springboot/backpacking/src/main/java/group61/backpacking/BackPackingController.java
@@ -3,9 +3,6 @@ package group61.backpacking;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.IntToDoubleFunction;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
