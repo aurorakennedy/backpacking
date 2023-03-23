b355f56dbdecce3f6b6162e676c479ab8d5564df
Jarl van Sterkenburg <jjsterke@stud.ntnu.no>
2023-03-23 19:09:41 +0100

    style: Minor style fixing
    Color and size of comment box.

--- ui/bp-advisor/src/components/AddComment.tsx
+++ ui/bp-advisor/src/components/AddComment.tsx
@@ -1,53 +1,53 @@
-import React, { useState, useRef } from 'react';
-import styles from './AddComment.module.css';
+import React, { useState, useRef } from "react";
+import styles from "./AddComment.module.css";
 
 interface AddCommentProps {
-  onSubmit: (comment: string) => void;
+    onSubmit: (comment: string) => void;
 }
 
 const AddComment: React.FC<AddCommentProps> = ({ onSubmit }) => {
-  const [comment, setComment] = useState('');
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
+    const [comment, setComment] = useState("");
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setComment(event.target.value);
+    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setComment(event.target.value);
 
-    if (textareaRef.current) {
-      textareaRef.current.style.height = 'auto';
-      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
-    }
-  };
+        if (textareaRef.current) {
+            textareaRef.current.style.height = "auto";
+            textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+        }
+    };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    onSubmit(comment);
-    setComment('');
-  };
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        onSubmit(comment);
+        setComment("");
+    };
 
-  const isCommentEmpty = comment.trim().length === 0;
+    const isCommentEmpty = comment.trim().length === 0;
 
-  return (
-    <form className={styles.addComment} onSubmit={handleSubmit}>
-      <label>
-        Add a comment:
-        <textarea
-          className={styles.commentTextarea}
-          value={comment}
-          onChange={handleChange}
-          ref={textareaRef}
-          style={{ height: '50px' }}
-        />
-      </label>
-      <button
-        className={styles.submitButton}
-        type="submit"
-        disabled={isCommentEmpty}
-        style={{ backgroundColor: isCommentEmpty ? '#ccc' : undefined }}
-      >
-        Submit
-      </button>
-    </form>
-  );
+    return (
+        <form className={styles.addComment} onSubmit={handleSubmit}>
+            <label>
+                Add a comment:
+                <textarea
+                    className={styles.commentTextarea}
+                    value={comment}
+                    onChange={handleChange}
+                    ref={textareaRef}
+                    style={{ height: "15px" }}
+                />
+            </label>
+            <button
+                className={styles.submitButton}
+                type="submit"
+                disabled={isCommentEmpty}
+                style={{ backgroundColor: isCommentEmpty ? "#ccc" : undefined }}
+            >
+                Submit
+            </button>
+        </form>
+    );
 };
 
 export default AddComment;
